53bfc0bba0bec7e924c479b754533a2d0a6ca083
kimk3931 <kimberlykelsy2005@gmail.com>
2024-05-05 21:05:14 +0300

    4th commit

--- tempCodeRunnerFile.php
+++ tempCodeRunnerFile.php
@@ -44,11 +44,11 @@ if(isset($_GET['delete'])){
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>admin page</title>
 
-   <!-- font awesome cdn link  -->
+  
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+   
+   <link rel="stylesheet" href="login.css">
 
 </head>
 <body>
