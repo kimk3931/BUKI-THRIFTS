75142ac62890908c920ecef29049b76d9ac7a92d
Starnoh254 <stajohn697@gmail.com>
2024-05-08 19:37:21 +0300

    added the products endpoints , tested them also ...

--- BackEnd/routes/products.js
+++ BackEnd/routes/products.js
@@ -20,4 +20,42 @@ router.post("/add_product", function (req, res) {
     });
 });
 
+router.post("/delete_product", function (req, res) {
+  const productId = req.body.product_id;
+
+  Products.deleteOne({ product_id: productId }) 
+    .then((result) => {
+      if (result.deletedCount === 1) {
+        res.status(200).json({ message: "Product deleted successfully" });
+        console.log(`Product deleted successfully: ${productId}`);
+      } else {
+        res.status(404).json({ message: "Product not found" });
+        console.log(`Product not found: ${productId}`);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Error in deleting product" });
+      console.error(err);
+    });
+});
+
+router.get("/get_products" , function(req,res){
+  Products.find({})
+    .then((results) => {
+      if (results.length > 0) {
+        res.status(200).json({ message: "Products fetched successfully", products: results });
+        console.log(`Products fetched successfully: ${results}`);
+      }
+      else{
+        res.status(404).json({ message: "No products found" });
+        console.log(`No products found`);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Error in fetching products" });
+      console.error(err);
+    })
+})
+
+
 module.exports = router;
