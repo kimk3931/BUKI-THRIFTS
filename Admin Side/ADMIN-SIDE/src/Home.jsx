1b7f41557bf68ca8a3177388cf1b1404268a6cb4
kimk3931 <kimberlykelsy2005@gmail.com>
2024-05-08 21:56:11 +0300

    I have created the admin side, but have not created the side bar links yet

---
+++ Admin Side/ADMIN-SIDE/src/Home.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import 
+{ BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsCash, BsReceipt}
+ from 'react-icons/bs'
+ 
+
+function Home() {
+
+   
+     
+
+  return (
+    <main className='main-container'>
+        <div className='main-title'>
+            <h3>DASHBOARD</h3>
+        </div>
+
+        <div className='main-cards'>
+            <div className='card'>
+                <div className='card-inner'>
+                    <h3>PRODUCTS</h3>
+                    <BsFillArchiveFill className='card_icon'/>
+                </div>
+                <h1>300</h1>
+            </div>
+            <div className='card'>
+                <div className='card-inner'>
+                    <h3>ORDERS</h3>
+                    <BsReceipt className='card_icon'/>
+                </div>
+                <h1>50</h1>
+            </div>
+            <div className='card'>
+                <div className='card-inner'>
+                    <h3>CUSTOMERS</h3>
+                    <BsPeopleFill className='card_icon'/>
+                </div>
+                <h1>33</h1>
+            </div>
+            <div className='card'>
+                <div className='card-inner'>
+                    <h3>REVENUE TOTAL</h3>
+                    <BsCash className='card_icon'/>
+                </div>
+                <h1> KSH 333,000</h1>
+            </div>
+           
+        </div>
+
+     
+    </main>
+  )
+}
+
+export default Home
---
+++ Admin Side/ADMIN-SIDE/src/Sidebar.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import 
+{BsCart3, BsGrid1X2Fill, BsFillArchiveFill, BsReceipt, BsPeopleFill, 
+  BsListCheck, BsMenuButtonWideFill, BsFillGearFill}
+ from 'react-icons/bs'
+
+function Sidebar({openSidebarToggle, OpenSidebar}) {
+  return (
+    <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
+        <div className='sidebar-title'>
+            <div className='sidebar-brand'>
+                <BsCart3  className='icon_header'/> BUKI THRIFT
+            </div>
+            <span className='icon close_icon' onClick={OpenSidebar}>X</span>
+        </div>
+
+        <ul className='sidebar-list'>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsGrid1X2Fill className='icon'/> Dashboard
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsFillArchiveFill className='icon'/> Products
+                </a>
+            </li>
+            
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsPeopleFill className='icon'/> Customers
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsReceipt className='icon'/> Orders
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsMenuButtonWideFill className='icon'/> Reports
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsFillGearFill className='icon'/> Settings
+                </a>
+            </li>
+        </ul>
+    </aside>
+  )
+}
+
+export default Sidebar
